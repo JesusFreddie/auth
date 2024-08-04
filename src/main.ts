0d20f31dfcce13516d32529710ff89295ceb6520
Constantine Sazonov <svedinkvidon@gmail.com>
2024-08-04 10:21:31 +0500

    add-prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function Start() {
+	const _PORT = 5000;
+	const app = await NestFactory.create(AppModule)
+	app.listen(_PORT, () => {
+		console.log(`Server started on port ${_PORT}`)
+	})
 }
-bootstrap();
+Start();
